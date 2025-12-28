8af9e92651fb5c3ca53f3c8b34d7ff93fb9cbf0a
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-28 11:35:11 +0530

    refactor: update backend configuration and improve server setup
    - Simplified package.json by removing unnecessary fields and added dotenv for environment variable management.
    - Updated index.ts to load environment variables and set a default port for the server.
    - Refined tsconfig.json to streamline compiler options and removed redundant settings.
    - Added tsconfig.tsbuildinfo for TypeScript build information.

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is runing on port: ${PORT}`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT, () => {
-    console.log(`Server is runing on port: ${process.env.PORT}`)
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server is runing on port: ${PORT}`)
 })
