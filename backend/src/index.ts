8b7c9857063526bdce4afb9d3031275fdd2e6d9e
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-28 19:38:35 +0530

    feat: integrate socket.io for real-time communication and update package dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,38 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 
+dotenv.config();
+
+app.use(cors());
+
+
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+}
+);
+
+
+io.on("connection", (socket)=>{
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("disconnect", ()=>{
+        console.log(`User disconnected: ${socket.id}`);
+    })
+})
+
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-    console.log(`Server is runing on port: ${PORT}`)
-})
+  console.log(`Server is runing on port: ${PORT}`);
+});
