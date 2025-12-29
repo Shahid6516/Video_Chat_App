458995680335a3c23e7e9c8b65ee7096573fda72
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-29 23:59:42 +0530

    feat: initialize user state with Peer in SocketContext and import UUIDv4

--- frontend/src/Context/SocketContext.tsx
+++ frontend/src/Context/SocketContext.tsx
@@ -2,6 +2,7 @@ import { io, Socket } from "socket.io-client";
 import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Peer from "peerjs";
+import { v4 as UUIDv4 } from "uuid";
 
 const socket: Socket = io("http://localhost:5000", {
   transports: ["websocket", "polling"],
@@ -12,9 +13,12 @@ export const SocketContext = createContext<any>(null);
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const navigate = useNavigate();
 
-  const [user, setUser]= useState<Peer>();
+  const [user, setUser] = useState<Peer>();
 
   useEffect(() => {
+    const userID = UUIDv4();
+    const newPeer = new Peer(userID);
+    setUser(newPeer);
     const enterRoom = ({ roomId }: { roomId: string }) => {
       navigate(`/room/${roomId}`);
     };
