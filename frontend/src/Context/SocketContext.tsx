8b032aa8fcb69b64aaa9e8d4846251e7ceee05a9
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-29 18:36:45 +0530

    feat: implement Room component and update routing to display room details

--- frontend/src/Context/SocketContext.tsx
+++ frontend/src/Context/SocketContext.tsx
@@ -1,13 +1,28 @@
-import { io } from "socket.io-client";
-import React, { createContext } from "react";
+import { io, Socket } from "socket.io-client";
+import React, { createContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket", "polling"], // optional
+const socket: Socket = io("http://localhost:5000", {
+  transports: ["websocket", "polling"],
 });
 
 export const SocketContext = createContext<any>(null);
 
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const enterRoom = ({ roomId }: { roomId: string }) => {
+      navigate(`/room/${roomId}`);
+    };
+
+    socket.on("room-created", enterRoom);
+
+    return () => {
+      socket.off("room-created", enterRoom);
+    };
+  }, [navigate]);
+
   return (
     <SocketContext.Provider value={{ socket }}>
       {children}
