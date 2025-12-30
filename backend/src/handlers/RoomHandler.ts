f995dad48dc573d38abc6d038bc3f01a9eeeb361
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-31 00:08:39 +0530

    refactor: clean up roomHandler logic and improve console logging

--- backend/src/handlers/RoomHandler.ts
+++ backend/src/handlers/RoomHandler.ts
@@ -1,40 +1,33 @@
 import { Socket } from "socket.io";
 import { v4 as UUIDv4 } from "uuid";
-import IRoomParams from "../interfaces/iRoomParams.js";
 
 const rooms: Record<string, string[]> = {};
+
 const roomHandler = (socket: Socket) => {
-
-
     const createRoom = () => {
         const roomId = UUIDv4();
+        rooms[roomId] = [];
         socket.join(roomId);
 
-        rooms[roomId] = [];
-
-
         socket.emit("room-created", { roomId });
-        console.log("room created with id:", socket.id)
-    }
+        console.log("Room created with id:", roomId);
+    };
 
     const joinedRoom = ({ roomId, peerId }: { roomId: string; peerId: string }) => {
-
         if (rooms[roomId]) {
-
-            console.log("New Room Joined:", roomId, "by peerId:", peerId);
+            console.log("New User Joined:", roomId, "Peer:", peerId);
             rooms[roomId].push(peerId);
             socket.join(roomId);
 
             socket.emit("get-users", {
                 roomId,
                 participants: rooms[roomId]
-            })
+            });
         }
+    };
 
-        socket.on("create-room", createRoom);
-        socket.on("joined-room", joinedRoom);
+    socket.on("create-room", createRoom);
+    socket.on("joined-room", joinedRoom);
+};
 
-    }
-    }
-
-    export default roomHandler;
+export default roomHandler;
