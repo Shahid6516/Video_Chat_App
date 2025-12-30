265bdf1e4687a45661d15d8215bb3bbbb66efe85
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-31 05:02:14 +0530

    feat: implement peerReducer to manage peer state in the application

--- frontend/src/Reducers/peerReducers.ts
+++ frontend/src/Reducers/peerReducers.ts
@@ -1,3 +1,32 @@
+import Stream from "stream";
 import { ADD_PEER, REMOVE_PEER } from "../Actions/peerAction";
 
 export type PeerState = Record<string, MediaStream>;
+
+type PeerAction ={
+    type: typeof ADD_PEER;
+    payload: { peerId: string; stream: MediaStream };
+} | {
+    type: typeof REMOVE_PEER;
+    payload: { peerId: string };
+}
+
+export const peerReducer = (state:PeerState, action:PeerAction)=>{
+    switch(action.type){
+        case ADD_PEER:
+            return{
+                ...state,
+                [action.payload.peerId]: {
+                    Stream: action.payload.stream
+                }
+            }
+            
+        case REMOVE_PEER:
+            const newState = { ...state };
+            delete newState[action.payload.peerId];
+            return newState;
+
+        default:
+            return state;
+    }
+}
