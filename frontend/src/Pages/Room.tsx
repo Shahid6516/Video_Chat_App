a961e0181a0e668110b30dcda5400def4e8cb62b
Mohammad Shahid <www.shahid6516@gmail.com>
2025-12-31 18:50:38 +0530

    fix: correct import formatting in Room component

--- frontend/src/Pages/Room.tsx
+++ frontend/src/Pages/Room.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import  { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../Context/SocketContext";
 import UserFeedPlayer from "../Component/UserFeedPlayer";
